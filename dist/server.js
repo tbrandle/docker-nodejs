01ec2355638650f0f294841a8c40402a3534f737
Tim Brandle <tbrandle@ritchiebros.com>
2024-03-07 14:05:53 -0700

    convert resume to type json

--- dist/server.js
+++ dist/server.js
@@ -15,99 +15,80 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
-class App {
-    constructor() {
-        this.app = (0, express_1.default)();
-        this.routes();
-        this.app.use(express_1.default.json());
-    }
-    routes() {
-        this.app.route("/").get((req, res) => __awaiter(this, void 0, void 0, function* () {
-            try {
-                const allResumes = yield prisma.resume.findMany({
-                    include: {
-                        personal_details: {
-                            include: {
-                                fields: true,
-                            },
-                        },
-                    },
-                });
-                res.status(200).send(allResumes);
-            }
-            catch (error) {
-                console.log(error);
-                res.sendStatus(500);
-            }
-        }));
-        this.app.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
-            try {
-                const allResumes = yield prisma.resume.findMany({
-                    include: {
-                        personal_details: {
-                            include: {
-                                fields: true,
-                            },
-                        },
-                    },
-                });
-                res.status(200).send(allResumes);
-            }
-            catch (error) {
-                console.log(error);
-                res.sendStatus(500);
-            }
-        }));
-        this.app.get("/:id", (req, res) => __awaiter(this, void 0, void 0, function* () {
-            try {
-                const resume = yield prisma.resume.findUnique({
-                    where: { id: req.params.id },
-                    include: {
-                        personal_details: {
-                            include: {
-                                fields: true
-                            },
-                        },
-                    },
-                });
-                res.status(200).send(resume);
-            }
-            catch (error) {
-                console.log(error);
-                res.sendStatus(500);
-            }
-        }));
-        this.app.post("/resume", (req, res) => __awaiter(this, void 0, void 0, function* () {
-            console.log("req.body", req.body);
-            const { resume_title, social_media, skills, employment_history, education, personal_details, } = req.body;
-            try {
-                const resume = yield prisma.resume.create({
-                    data: {
-                        resume_title,
-                        social_media,
-                        skills,
-                        employment_history,
-                        education,
-                        personal_details: {
-                            create: Object.assign(Object.assign({}, personal_details), { fields: {
-                                    createMany: {
-                                        data: personal_details.fields,
-                                    },
-                                } }),
-                        },
-                    },
-                });
-                res
-                    .status(200)
-                    .send({ message: `Successfully added resume`, id: resume.id });
-            }
-            catch (error) {
-                console.log(error);
-                res.sendStatus(500);
-            }
-        }));
-    }
-}
 const port = 8080;
-const app = new App().app;
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get("/resumes", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const allResumes = yield prisma.resume.findMany();
+        res.status(200).send(allResumes);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}));
+app.get("/resumes/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const resume = yield prisma.resume.findUnique({
+            where: { id: req.params.id },
+        });
+        res.status(200).send(resume);
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}));
+app.post("/resumes", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const resume = yield prisma.resume.create({
+            data: {
+                resume: req.body,
+            },
+        });
+        res
+            .status(200)
+            .send({ message: `Successfully added resume`, id: resume.id });
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}));
+app.patch("/resumes/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const resume = yield prisma.resume.update({
+            data: {
+                resume: req.body,
+            },
+            where: {
+                id: req.params.id,
+            },
+        });
+        res
+            .status(200)
+            .send({ message: `Successfully updated resume`, id: resume.id });
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}));
+app.delete("/resumes/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const resume = yield prisma.resume.delete({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res
+            .status(200)
+            .send({ message: `Successfully deleted resume`, id: resume.id });
+    }
+    catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}));
 app.listen(port, () => console.log(`Server has started on port ${port}`));
--- server.ts
+++ server.ts
@@ -1,115 +1,85 @@
-import express, { Application } from "express";
+import express from "express";
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
-
-class App {
-  public app: Application;
-
-  constructor() {
-    this.app = express();
-    this.routes();
-    this.app.use(express.json());
-  }
-
-  protected routes(): void {
-    this.app.route("/").get(async (req, res) => {
-      try {
-        const allResumes = await prisma.resume.findMany({
-          include: {
-            personal_details: {
-              include: {
-                fields: true,
-              },
-            },
-          },
-        });
-        res.status(200).send(allResumes);
-      } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-      }
-    });
-
-    this.app.get("/", async (req, res) => {
-      try {
-        const allResumes = await prisma.resume.findMany({
-          include: {
-            personal_details: {
-              include: {
-                fields: true,
-              },
-            },
-          },
-        });
-        res.status(200).send(allResumes);
-      } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-      }
-    });
-
-    this.app.get("/:id", async (req, res) => {
-      try {
-        const resume = await prisma.resume.findUnique({
-          where: { id: req.params.id },
-          include: {
-            personal_details: {
-              include: {
-                fields: true
-              },
-            },
-          },
-        });
-        res.status(200).send(resume);
-      } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-      }
-    });
-
-    this.app.post("/resume", async (req, res) => {
-      console.log("req.body", req.body);
-      const {
-        resume_title,
-        social_media,
-        skills,
-        employment_history,
-        education,
-        personal_details,
-      } = req.body;
-
-      try {
-        const resume = await prisma.resume.create({
-          data: {
-            resume_title,
-            social_media,
-            skills,
-            employment_history,
-            education,
-            personal_details: {
-              create: {
-                ...personal_details,
-                fields: {
-                  createMany: {
-                    data: personal_details.fields,
-                  },
-                },
-              },
-            },
-          },
-        });
-        res
-          .status(200)
-          .send({ message: `Successfully added resume`, id: resume.id });
-      } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-      }
-    });
-  }
-}
 
 const port = 8080;
 
-const app = new App().app;
+const app = express();
+app.use(express.json());
+
+app.get("/resumes", async (req, res) => {
+  try {
+    const allResumes = await prisma.resume.findMany();
+    res.status(200).send(allResumes);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.get("/resumes/:id", async (req, res) => {
+  try {
+    const resume = await prisma.resume.findUnique({
+      where: { id: req.params.id },
+    });
+    res.status(200).send(resume);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/resumes", async (req, res) => {
+  try {
+    const resume = await prisma.resume.create({
+      data: {
+        resume: req.body,
+      },
+    });
+    res
+      .status(200)
+      .send({ message: `Successfully added resume`, id: resume.id });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.patch("/resumes/:id", async (req, res) => {
+  try {
+    const resume = await prisma.resume.update({
+      data: {
+        resume: req.body,
+      },
+      where: {
+        id: req.params.id,
+      },
+    });
+    res
+      .status(200)
+      .send({ message: `Successfully updated resume`, id: resume.id });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.delete("/resumes/:id", async (req, res) => {
+  try {
+    const resume = await prisma.resume.delete({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res
+      .status(200)
+      .send({ message: `Successfully deleted resume`, id: resume.id });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
 app.listen(port, () => console.log(`Server has started on port ${port}`));
